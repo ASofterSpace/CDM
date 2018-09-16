98b0a9052fe2d1cd9fa3ea741e5d9949266799d6
Moya <moyaccercchi@hotmail.de>
2018-09-16 20:32:03 +0200

    Add CDM creation

--- src/com/asofterspace/cdm/Main.java
+++ src/com/asofterspace/cdm/Main.java
@@ -16,11 +16,11 @@ import java.util.List;
 public class Main {
 
 	public final static String PROGRAM_TITLE = "cdm commandline tool";
-	public final static String VERSION_NUMBER = "0.0.0.3(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
-	public final static String VERSION_DATE = "13. September 2018 - 14. September 2018";
+	public final static String VERSION_NUMBER = "0.0.0.4(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
+	public final static String VERSION_DATE = "13. September 2018 - 16. September 2018";
 
 	public static void main(String[] args) {
-	
+
 		// let the Utils know in what program it is being used
 		Utils.setProgramTitle(PROGRAM_TITLE);
 		Utils.setVersionNumber(VERSION_NUMBER);
@@ -32,10 +32,18 @@ public class Main {
 			showHelp();
 			System.exit(1);
 		}
-		
+
 		// check all the arguments
 		String lowarg = args[0].toLowerCase();
 		switch (lowarg) {
+			case "create":
+				if (args.length > 3) {
+					createCdm(args[1], args[2], args[3]);
+				} else {
+					System.err.println("You called  cdm create  which should be followed by a template, a version and a path, but did not specify these arguments.");
+					System.exit(4);
+				}
+				break;
 			case "info":
 				if (args.length > 1) {
 					showInfo(args[1]);
@@ -72,22 +80,65 @@ public class Main {
 				System.err.println("Sorry, I did not understand the argument '" + args[0] + "' - call  cdm help  to get a list of possible commands.");
 				System.exit(2);
 		}
-		
+
 		// all is shiny! all is good! exit code 0!
 		System.exit(0);
 	}
-	
+
 	private static void loadCdm(String pathArg) {
-		
+
 		Directory cdmDir = new Directory(pathArg);
 		ProgressIndicator noProgress = new NoOpProgressIndicator();
-		
+
 		try {
 			CdmCtrl.loadCdmDirectory(cdmDir, noProgress);
 		} catch (AttemptingEmfException | CdmLoadingException e) {
 			System.err.println(e.getMessage());
 			System.exit(3);
 		}
+	}
+
+	private static void createCdm(String template, String targetVersionAndPrefix, String path) {
+
+		if ("-".equals(template)) {
+			template = CdmCtrl.getTemplates().get(0);
+		}
+
+		String[] verPrefArgs = targetVersionAndPrefix.split(":");
+		String version;
+		String prefix;
+
+		if (verPrefArgs.length > 1) {
+			prefix = verPrefArgs[0];
+			version = verPrefArgs[1];
+		} else {
+			prefix = "-";
+			version = verPrefArgs[0];
+		}
+
+		if ("-".equals(version)) {
+			version = CdmCtrl.getHighestKnownCdmVersion();
+		}
+
+		// if no prefix is specified, take the correct one automagically!
+		if ("-".equals(prefix)) {
+			prefix = CdmCtrl.getPrefixForVersion(version);
+			if (prefix == null) {
+				System.err.println("I do not know which prefix is associated with CDM version " + version + ".");
+				System.err.println("Please explicitly specify a prefix, e.g. call  cdm create (...) prefix:" + version + " (...)");
+				System.exit(7);
+			}
+		}
+
+		try {
+			CdmCtrl.createNewCdm(path, version, prefix, template);
+
+		} catch (AttemptingEmfException | CdmLoadingException e) {
+			System.err.println(e.getMessage());
+			System.exit(8);
+		}
+		
+		System.out.println("The new CDM has been created!");
 	}
 
 	private static void showInfo(String pathArg) {
@@ -102,20 +153,20 @@ public class Main {
 		System.out.println("CDM compatible with EGS-CC release: " + CdmCtrl.getCompatWithEGSCCstr(cdmVersion, cdmPrefix));
 		System.out.println("CDM compatible with RTF Framework CDM editor version: " + CdmCtrl.getCompatWithMCDEstr(cdmVersion, cdmPrefix));
 	}
-	
+
 	/**
 	 * Open the CDM in originPath and convert it to the specified version (possibly including a prefix separated with a colon) and format.
 	 * If destination path is null, overwrite the CDM in place.
 	 * If destination path is given, store the conversion result there.
 	 */
 	private static void convertCdm(String targetVersionAndPrefix, String targetFormat, String originPath, String destinationPath) {
-	
+
 		String conversionTargetStr = "";
-	
+
 		// first check if the target format selected actually can be done (and if not, complain immediately!)
 		if (!"-".equals(targetFormat)) {
 			targetFormat = targetFormat.toUpperCase();
-			
+
 			switch (targetFormat) {
 				case "XML":
 					break;
@@ -123,10 +174,10 @@ public class Main {
 					System.err.println("Sorry, the target format " + targetFormat + " is not yet supported!");
 					System.exit(5);
 			}
-		
+
 			conversionTargetStr = targetFormat + " format";
 		}
-		
+
 		// now attempt to load the CDM from the origin path
 		loadCdm(originPath);
 
@@ -135,40 +186,40 @@ public class Main {
 			String[] verPrefArgs = targetVersionAndPrefix.split(":");
 			String toVersion = null;
 			String toPrefix = null;
-			
+
 			if (verPrefArgs.length > 1) {
 				toPrefix = verPrefArgs[0];
 				toVersion = verPrefArgs[1];
 			} else {
 				toVersion = verPrefArgs[0];
 			}
-			
+
 			if ("-".equals(toVersion)) {
 				toVersion = null;
 			}
-			
+
 			// if no prefix is specified, take the correct one automagically!
 			// (however, if the target version is null - so kept the same - then the prefix can also be null - also be kept the same)
 			if ((toPrefix == null) && (toVersion != null)) {
 				toPrefix = CdmCtrl.getPrefixForVersion(toVersion);
 				if (toPrefix == null) {
 					System.err.println("I do not know which prefix is associated with CDM version " + toVersion + ".");
-					System.err.println("Please explicitly specify a prefix, e.g. call  cdm convert -:" + toVersion + " (...)");
+					System.err.println("Please explicitly specify a prefix (even '-' would be enough to keep the current one), e.g. call  cdm convert -:" + toVersion + " (...)");
 					System.exit(7);
 				}
 			}
-			
+
 			// only check this now, after having checked for null, as when the user really does enter "-:...",
 			// then we know they want to keep the original prefix and only change the version!
 			if ("-".equals(toPrefix)) {
 				toPrefix = null;
 			}
-			
+
 			// we have to check again, as the user could have entered not just "-" (which would have triggered the outer if),
 			// but also "-:-" (which we are guarding against here) to be especially funny... ;)
 			if ((toPrefix != null) || (toVersion != null)) {
 				CdmCtrl.convertTo(toVersion, toPrefix);
-				
+
 				if ("".equals(conversionTargetStr)) {
 					conversionTargetStr = "CDM version " + CdmCtrl.getCdmVersion();
 				} else {
@@ -176,37 +227,37 @@ public class Main {
 				}
 			}
 		}
-		
+
 		// now actually save the result
 		if ((destinationPath == null) || ("-".equals(destinationPath))) {
-		
+
 			// overwrite the original with the new result
 			// TODO :: do not ignore the target format once we have more than XML available!
 			CdmCtrl.save();
-		
+
 		} else {
-		
+
 			// save the result to the new destination path
 			// TODO :: do not ignore the target format once we have more than XML available!
 			CdmCtrl.saveTo(new Directory(destinationPath));
 		}
-		
+
 		if ("".equals(conversionTargetStr)) {
 			System.out.println("No conversion done - as I was told to keep both version and format the same! :)");
 		} else {
 			System.out.println("Conversion to " + conversionTargetStr + " done!");
 		}
 	}
-	
+
 	private static void validate(String pathArg) {
 
 		// TODO :: if this is just one file (e.g. toLowerCase() ends on .cdm) then actually just load that one file instead!
 		loadCdm(pathArg);
-		
+
 		List<String> problems = new ArrayList<>();
-		
+
 		int problemAmount = CdmCtrl.checkValidity(problems);
-		
+
 		if (problemAmount > 0) {
 			System.err.println("The CDM does not seem to be valid.");
 			if (problemAmount == 1) {
@@ -220,7 +271,7 @@ public class Main {
 			}
 			System.exit(6);
 		}
-		
+
 		System.out.println("The CDM looks valid to me!");
 	}
 
@@ -229,7 +280,20 @@ public class Main {
 		System.out.println("");
 		System.out.println("Available commands:");
 		System.out.println("");
-		// TODO :: add create <path> [<template>] (e.g. just empty MCM root - for this, see the tinyCdm template, or MCM root with default route and sap - as we currently are doing, or MCM root with route, sap and an example activity and an example argument type)
+		System.out.println("create <template> <[targetVersionPrefix:]targetVersion> <path> .. creates a new CDM of the specified version at the specified path, using one of the available templates for it");
+		System.out.println("  available templates:");
+		List<String> templates = CdmCtrl.getTemplates();
+		List<String> templatesShort = CdmCtrl.getTemplatesShort();
+		for (int i = 0; i < templates.size(); i++) {
+			System.out.println("    " + templatesShort.get(i) + " .. " + templates.get(i));
+		}
+		System.out.println("    - .. default template (" + templates.get(0) + ")");
+		System.out.println("  supported versions:");
+		for (String ver : CdmCtrl.getKnownCdmVersions()) {
+			System.out.println("    " + ver);
+		}
+		System.out.println("    - .. highest available version (" + CdmCtrl.getHighestKnownCdmVersion() + ")");
+		System.out.println("");
 		System.out.println("info <path> .. shows information about the CDM lying at the path");
 		System.out.println("");
 		System.out.println("convert <[targetVersionPrefix:]targetVersion> <targetFormat> <sourcePath> [<destinationPath>] .. converts the CDM lying at the source path to the target version (optionally including a custom prefix, by default using the correct one) and format and saves it in the destination path, or overwrites the CDM on disk if no destination path is given");
@@ -253,7 +317,7 @@ public class Main {
 		System.out.println("");
 		System.out.println("help .. shows this help");
 	}
-	
+
 	private static void showVersion() {
 		System.out.println(Utils.getFullProgramIdentifierWithDate());
 	}
